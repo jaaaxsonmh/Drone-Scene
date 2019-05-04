17c66300b3e2f9e02f8aeca6168b500d5eefa222
Jack hosking <jackhosking2@outlook.com>
2019-05-04 22:28:41 +1200

    fix clamping issue, changed to clamp to edge

--- src/objects/Skybox.java
+++ src/objects/Skybox.java
@@ -29,12 +29,12 @@ public class Skybox implements Drawable {
     }
 
     private void drawTexture(GL2 gl) {
-        skyDome.enable(gl);
+        gl.glEnable(GL2.GL_TEXTURE_2D);
         skyDome.bind(gl);
-
-        skyDome.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_S, GL2.GL_CLAMP);
-        skyDome.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_T, GL2.GL_CLAMP);
-
+        skyDome.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_S, GL2.GL_CLAMP_TO_EDGE);
+        skyDome.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_T, GL2.GL_CLAMP_TO_EDGE);
+        skyDome.setTexParameteri(gl, GL2.GL_TEXTURE_MIN_FILTER, GL2.GL_NEAREST);
+        skyDome.setTexParameteri(gl, GL2.GL_TEXTURE_MAG_FILTER, GL2.GL_LINEAR);
     }
 
     private void drawMaterial(GL2 gl) {
@@ -45,7 +45,6 @@ public class Skybox implements Drawable {
         gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_SPECULAR, greyMaterial, 0);
         gl.glMaterialf(GL2.GL_FRONT, GL2.GL_SHININESS, 50.0f);
     }
-
 
     @Override
     public void draw(GL2 gl, GLU glu, GLUquadric quadric, boolean filled) {
@@ -60,7 +59,7 @@ public class Skybox implements Drawable {
         gl.glRotated(180, 0, 1, 0);
         gl.glRotated(-90, 1, 0, 0);
         glu.gluQuadricTexture(quadric, true);
-        glu.gluSphere(quadric, 1.0, 50, 40);
+        glu.gluSphere(quadric, 1.0, 40, 40);
 
         gl.glPopMatrix();
         skyDome.disable(gl);
